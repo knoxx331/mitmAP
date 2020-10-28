2c9dd7592868ee9e0e6e523c73e743dd40794d76
knoxx332 <mayank002652@gmail.com>
2020-10-28 16:28:42 +0530

    code added

---
+++ 263A.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int x;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> x;
+            if (x == 1) {
+                cout << abs(2 - i) + abs(2 - j) << endl;
+                return 0;
+            }
+        }
+    }
+}
