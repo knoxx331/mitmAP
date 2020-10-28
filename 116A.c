fc8fc81835b0c163924f1cfe34728a024bb8389a
knoxx332 <mayank002652@gmail.com>
2020-10-28 16:00:12 +0530

    cpp added

---
+++ 116A.c
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define FR(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int c = 0, mx = 0;
+    FR(i, n) {
+
+        int a, b;
+        cin >> a >> b;
+        c -= a;
+        c += b;
+
+        mx = max(mx, c);
+    }
+
+    cout << mx << endl;
+}
+
+
